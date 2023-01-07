8e62d7a3b5abfc650cf40b06dda0d6a6b7189988
ptg0811 <ptg0811@gmail.com>
2023-01-07 21:00:41 +0900

    FEAT: modify componentization of social login #4

---
+++ src/components/users/signup/NaverSignupButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Button from '../../common/Button';
+import { Colors } from '../../../styles/colors';
+
+import NaverLogo from '../../../assets/icons/ico_Naver_logo.png';
+
+// TODO: media query 설정
+// TODO: redirect uri, client id env 파일 설정
+const NaverSignupButton = () => {
+  // TODO: state 추후 변경 필요
+  const naverStateString = 'test';
+
+  const handleNaverSignup = () => {
+    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&state=${naverStateString}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+  };
+
+  return (
+    <>
+      <Button
+        size='large'
+        background={Colors.naver}
+        color='white'
+        onClick={handleNaverSignup}>
+        <Img src={NaverLogo} />
+        NAVER로 계속하기
+      </Button>
+    </>
+  );
+};
+
+const Img = styled.img`
+  width: 20px;
+  height: 20px;
+  margin: 0px 10px;
+`;
+
+export default NaverSignupButton;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,59 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Button from '../components/common/Button';
-import { Colors } from '../styles/colors';
+import KakaoSignupButton from '../components/users/signup/KakaoSignupButton';
+import NaverSignupButton from '../components/users/signup/NaverSignupButton';
 
-import KakaoLogo from '../assets/icons/ico_KakaoTalk_logo.svg';
-import GoogleLogo from '../assets/icons/ico_Google_logo.svg';
-import NaverLogo from '../assets/icons/ico_Naver_logo.png';
+import GoogleSignupButton from '../components/users/signup/GoogleSignupButton';
 
 // TODO: media query 설정
-// TODO: 소셜로그인 로고
 // TODO: redirect uri, client id env 파일 설정
 const LandingPage = () => {
-  const kakaoSignup = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
-  };
-
-  // TODO: state 추후 변경 필요
-  const naverStateString = 'test';
-
-  const naverSignup = () => {
-    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&state=${naverStateString}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
-  };
-
-  const googleSignup = () => {
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
-  };
-
   return (
     <Wrapper>
-      <Button
-        size='large'
-        background={Colors.kakao}
-        color='black'
-        onClick={kakaoSignup}>
-        <Img src={KakaoLogo} />
-        Kakao talk으로 계속하기
-      </Button>
-      <Button
-        size='large'
-        background={Colors.naver}
-        color='white'
-        onClick={naverSignup}>
-        <Img src={NaverLogo} />
-        NAVER로 계속하기
-      </Button>
-      <Button
-        size='large'
-        background='white'
-        color='black'
-        border='2px solid'
-        onClick={googleSignup}>
-        <Img src={GoogleLogo} />
-        Google으로 계속하기
-      </Button>
+      <KakaoSignupButton />
+      <NaverSignupButton />
+      <GoogleSignupButton />
     </Wrapper>
   );
 };
@@ -63,12 +23,6 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const Img = styled.img`
-  width: 20px;
-  height: 20px;
-  margin: 0px 10px;
 `;
 
 export default LandingPage;
