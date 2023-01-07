79ca2865c25e94bec70edbd7726e98384eafa74b
ptg0811 <ptg0811@gmail.com>
2023-01-07 20:42:05 +0900

    FEAT: modify REDIRECT_URI #4

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,18 +13,18 @@ import NaverLogo from '../assets/icons/ico_Naver_logo.png';
 // TODO: redirect uri, client id env 파일 설정
 const LandingPage = () => {
   const kakaoSignup = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}`;
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
   };
 
   // TODO: state 추후 변경 필요
   const naverStateString = 'test';
 
   const naverSignup = () => {
-    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&state=${naverStateString}&redirect_uri=${process.env.REACT_APP_NAVER_CALLBACK_URL}`;
+    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&state=${naverStateString}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
   };
 
   const googleSignup = () => {
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
   };
 
   return (
