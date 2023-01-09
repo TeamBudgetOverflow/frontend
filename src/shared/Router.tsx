ba81d210debdcc8c982d8bff4372d305b00afe4f
ptg0811 <ptg0811@gmail.com>
2023-01-09 19:49:53 +0900

    FEAT: add Agreement of use of personel info #4

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AgreementOfCollectionPersonalInfo from '../pages/\bAgreementOfCollectionPersonalInfo';
 
 import LandingPage from '../pages/LandingPage';
 import PinNumberInputPage from '../pages/PinNumberInputPage';
@@ -12,6 +13,10 @@ const Router = () => {
         <Routes>
           <Route path='/' element={<LandingPage />} />
           <Route path='/pinnumber' element={<PinNumberInputPage />} />
+          <Route
+            path='/agreement'
+            element={<AgreementOfCollectionPersonalInfo />}
+          />
         </Routes>
       </Layout>
     </BrowserRouter>
