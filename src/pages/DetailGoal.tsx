ec53fa7b0531eb232e3d04fe1d9ce3fd58d1298a
unknown <sonewdim@gmail.com>
2023-01-10 23:03:43 +0900

    FEAT: add main page

---
+++ src/pages/DetailGoal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailGoal = () => {
+  return <div>Detail</div>;
+};
+
+export default DetailGoal;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Home from '../pages/Home';
+import AddGoal from '../pages/AddGoal';
+import DetailGoal from '../pages/DetailGoal';
 import Navigation from './Navigation';
 
 const Router = () => {
@@ -11,6 +13,8 @@ const Router = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/goal/add' element={<AddGoal />} />
+        <Route path='/goal/:id' element={<DetailGoal />} />
       </Routes>
       <Navigation />
     </BrowserRouter>
