c067c5a87c42f8158594d11324d2dfb61d76cc89
unknown <sonewdim@gmail.com>
2023-01-11 19:58:35 +0900

    FIX: modify input box to able size and border radius style customize

--- src/components/common/elem/InputBox.tsx
+++ src/components/common/elem/InputBox.tsx
@@ -5,21 +5,33 @@ interface InputBoxProps {
   value: string | number;
   placeholder: string;
   onChangeHandler: (e: React.FormEvent<HTMLInputElement>) => void;
+  borderRadius?: string;
 }
 
-const InputBox = ({ placeholder, value, onChangeHandler }: InputBoxProps) => {
+const InputBox = ({
+  placeholder,
+  value,
+  onChangeHandler,
+  borderRadius,
+}: InputBoxProps) => {
   return (
-    <Input value={value} placeholder={placeholder} onChange={onChangeHandler} />
+    <Input
+      value={value}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+      borderRadius={borderRadius}
+    />
   );
 };
 
-const Input = styled.input`
+const Input = styled.input<{ borderRadius?: string }>`
   padding: 0;
   width: 100%;
-  height: 30px;
+  height: 100%;
   border: 1px solid black;
   text-indent: 10px;
   font: ${(props) => props.theme.captionC3};
+  border-radius: ${(props) => props.borderRadius};
 `;
 
 export default InputBox;
