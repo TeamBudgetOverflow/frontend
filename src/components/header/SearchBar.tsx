0882b578bd068b1debfa008346a1c37a91e47b0a
unknown <sonewdim@gmail.com>
2023-01-16 21:29:54 +0900

    FIX: modify search bar component to able customize showing search bar

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-import Icon from '../common/elem/Icon';
 import InputBox from '../common/elem/InputBox';
 
-const SearchBar = () => {
+interface SearchBarProps {
+  show: boolean;
+}
+
+const SearchBar = ({ show }: SearchBarProps) => {
   const [searchKeyword, setSearchKeyword] = useState('');
   const navigate = useNavigate();
   const handleSearchButton = (e: string) => {
@@ -20,40 +23,32 @@ const SearchBar = () => {
   };
 
   return (
-    <SearchBarLayout>
-      <InputBox
-        type='text'
-        placeholder='검색'
-        borderRadius='20px'
-        onChangeHandler={(e) => setSearchKeyword(e.currentTarget.value)}
-        onKeyPressHandler={(e) => handleOnKeyPress(e)}
-      />
-      <SearchIconWrapper>
-        <Icon>
-          <path d='M10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6m13.12 2.88-4.26-4.26A9.842 9.842 0 0 0 20 10c0-5.52-4.48-10-10-10S0 4.48 0 10s4.48 10 10 10c1.67 0 3.24-.41 4.62-1.14l4.26 4.26a3 3 0 0 0 4.24 0 3 3 0 0 0 0-4.24'></path>
-        </Icon>
-      </SearchIconWrapper>
+    <SearchBarLayout show={show}>
+      <SearchInputWrapper show={show}>
+        <InputBox
+          type='text'
+          placeholder='검색어를 입력하세요'
+          onChangeHandler={(e) => setSearchKeyword(e.currentTarget.value)}
+          onKeyPressHandler={(e) => handleOnKeyPress(e)}
+          showBorder={false}
+        />
+      </SearchInputWrapper>
     </SearchBarLayout>
   );
 };
 
-const SearchBarLayout = styled.div`
-  flex: 1;
-  margin: 0 30px;
+const SearchBarLayout = styled.div<{ show: boolean }>`
+  width: ${(props) => (props.show ? '100%' : '0')};
   height: 60%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  transition: width 0.8s;
 `;
 
-const SearchIconWrapper = styled.div`
-  margin: 0px 5px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  :hover {
-    cursor: pointer;
-  }
+const SearchInputWrapper = styled.div<{ show: boolean }>`
+  padding: ${(props) => (props.show ? '4px 17px' : '0')};
+  width: calc(100% - 34px);
+  border-radius: 32px;
+  background-color: ${(props) => props.theme.gray300};
+  transition: padding 0.5s;
 `;
 
 export default SearchBar;
