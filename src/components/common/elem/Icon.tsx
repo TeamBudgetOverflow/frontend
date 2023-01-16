632978d1c943a346e39a708cc5f812dc9a780302
unknown <sonewdim@gmail.com>
2023-01-16 21:27:02 +0900

    FIX: modify icon atomic component to able customizing size

--- src/components/common/elem/Icon.tsx
+++ src/components/common/elem/Icon.tsx
@@ -1,13 +1,36 @@
-import React, { FunctionComponent, PropsWithChildren } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const Icon: FunctionComponent<PropsWithChildren> = ({ children }) => {
-  return <SVGIcon viewBox='0 0 24 24'>{children}</SVGIcon>;
+interface IconProps {
+  size: number;
+  color: string;
+  path: string;
+}
+
+const Icon = ({ size, color, path }: IconProps) => {
+  return (
+    <SVGIcon size={`${size}px`} viewBox={`0 0 ${size} ${size}`}>
+      <Path color={color} d={path} fillRule='evenodd' clipRule='evenodd' />
+    </SVGIcon>
+  );
 };
 
-const SVGIcon = styled.svg`
-  width: 1rem;
-  height: 1rem;
+const SVGIcon = styled.svg<{ size: string }>`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+`;
+
+const Path = styled.path<{ color: string }>`
+  fill: ${(props) => {
+    switch (props.color) {
+      case 'primary400':
+        return props.theme.primary400;
+      case 'gray400':
+        return props.theme.gray400;
+      default:
+        return props.theme.secondary900;
+    }
+  }};
 `;
 
 export default Icon;
