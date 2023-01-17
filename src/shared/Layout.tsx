90cf900a7f122c96658b743c8c8883f6e5f40090
unknown <sonewdim@gmail.com>
2023-01-17 17:12:50 +0900

    FIX: fix layout component to recalculate body element height on URL path change

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,18 +1,21 @@
 import React, { FunctionComponent, PropsWithChildren, useRef, useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import styled from 'styled-components';
 
 import Header from './Header';
 import Navigation from './Navigation';
 
 const Layout: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  const { pathname } = useLocation();
   const headerRef = useRef<HTMLDivElement>(null);
   const navRef = useRef<HTMLDivElement>(null);
 
   const [headerNavHeight, setHeaderNavHeight] = useState<number>(0);
   useEffect(() => {
     if (!headerRef.current || !navRef.current) return;
+    if (pathname === '/goals/post') return setHeaderNavHeight(headerRef.current.clientHeight);
     setHeaderNavHeight(headerRef.current.clientHeight + navRef.current.clientHeight);
-  }, [headerRef.current, navRef.current]);
+  }, [headerRef.current?.clientHeight, navRef.current?.clientHeight, pathname]);
   return (
     <>
       <Header props='' ref={headerRef} />
