84821fd29c11b7903bd502ddf5b0b8c1509dc52e
unknown <sonewdim@gmail.com>
2023-01-21 01:15:47 +0900

    FIX: fix state tage to adjust design

--- src/components/common/tag/StateTag.tsx
+++ src/components/common/tag/StateTag.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StateTag = ({ state }: { state: 'working' | 'recruiting' }) => {
-  return <Tag>{state === 'working' ? '진행중' : '모집중'}</Tag>;
+  return (
+    <Tag>
+      <StateCircle state={state} />
+      <Text state={state}>{state === 'working' ? '진행 예정' : '모집중'}</Text>
+    </Tag>
+  );
 };
 
 const Tag = styled.div`
-  font: ${(props) => props.theme.captionC3};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+`;
+
+const StateCircle = styled.div<{ state: string }>`
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.state === 'working' ? props.theme.primary700 : props.theme.secondary700)};
+`;
+
+const Text = styled.div<{ state: string }>`
+  font: ${(props) => props.theme.captionC2};
+  color: ${(props) => (props.state === 'working' ? props.theme.primary700 : props.theme.secondary700)};
 `;
 
 export default StateTag;
