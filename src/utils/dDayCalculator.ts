e703af6f32e416e0a8efaaef496a38e3bdcdf82c
unknown <sonewdim@gmail.com>
2023-01-24 01:13:52 +0900

    FIX: fix d day calculator to return d-day string on 0 value

--- src/utils/dDayCalculator.ts
+++ src/utils/dDayCalculator.ts
@@ -1,6 +1,7 @@
 export const dDayCalculator = (targetDate: Date): number => {
   const today = new Date().getTime();
-  const endDay = targetDate.getTime();
+  const endDay = targetDate.setHours(24);
   const dDay = Math.trunc((endDay - today) / (60000 * 60 * 24));
+
   return dDay;
 };
