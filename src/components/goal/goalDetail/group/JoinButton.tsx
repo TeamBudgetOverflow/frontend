1e3aca383b7cbf7b1a4ac19dab345a65a7d5dd68
unknown <sonewdim@gmail.com>
2023-01-28 03:27:53 +0900

    FIX: fix goal join button to disable account type select option on current version

--- src/components/goal/goalDetail/group/JoinButton.tsx
+++ src/components/goal/goalDetail/group/JoinButton.tsx
@@ -4,14 +4,20 @@ import styled from 'styled-components';
 import TextButton from '../../../common/elem/TextButton';
 import ModalBox from '../../../common/elem/ModalBox';
 import CloseIconBtn from '../../../common/elem/btn/CloseIconBtn';
-import ToggleSelectBox from '../../../common/elem/ToggleSelectBox';
+// TODO: 실계좌 기능 오픈
+// import ToggleSelectBox from '../../../common/elem/ToggleSelectBox';
 import AccountSelect from '../../../account/AccountSelectSection';
 
 import useAccountsData from '../../../../hooks/useAccountsData';
 import useJoinGoalModal from '../../../../hooks/useJoinGoalModal';
+import {
+  availAutoAccountFinder,
+  isAutoAccountAddable,
+  isManualAccountAddable,
+} from '../../../../utils/accountInfoChecker';
 
 const JoinButton = ({ goalId }: { goalId: number }) => {
-  const { isLoading, accounts, isError } = useAccountsData();
+  const { isLoading, isError, accounts } = useAccountsData();
 
   const {
     showOption,
@@ -19,7 +25,8 @@ const JoinButton = ({ goalId }: { goalId: number }) => {
     selectedAccntId,
     handleJoinStart,
     handleJoinEnd,
-    handleSelectOption,
+    // TODO: 실계좌 기능 오픈
+    // handleSelectOption,
     handleSelectOptionDone,
     handleSelectAccnt,
     handleSelectAccntDone,
@@ -34,28 +41,48 @@ const JoinButton = ({ goalId }: { goalId: number }) => {
       <ModalBox show={showOption}>
         <CloseIconBtn closeHandler={handleJoinEnd} />
         <Content>
-          <ToggleSelectBox
+          {isManualAccountAddable(accounts) ? (
+            <>
+              <Info>모은 금액은 직접 입력하여 업데이트를 해주세요.</Info>
+              {/* TODO: 실계좌 기능 오픈 */}
+              {/* <ToggleSelectBox
             title='계좌 잔액 직접 입력'
             description='계좌를 연결하지 않고 계좌 잔액을 직접 입력합니다.'
             initVal={false}
             selectHandler={handleSelectOption}
-          />
-          <TextButton text='다음' onClickHandler={handleSelectOptionDone} />
+          /> */}
+              <TextButton text='다음' onClickHandler={handleSelectOptionDone} />
+            </>
+          ) : (
+            <></>
+          )}
         </Content>
       </ModalBox>
       <ModalBox show={showAccounts}>
         <CloseIconBtn closeHandler={handleJoinEnd} />
         <Content>
-          {accounts.length === 0 ? (
-            <Info>연결된 계좌가 없습니다. 계좌를 연결하시겠습니까?</Info>
+          {isAutoAccountAddable(accounts) ? (
+            <>
+              {availAutoAccountFinder(accounts).length === 0 ? (
+                <Info>연결된 계좌가 없습니다. 계좌를 연결하시겠습니까?</Info>
+              ) : (
+                <AccountSelect accounts={availAutoAccountFinder(accounts)} accountSelectHandler={handleSelectAccnt} />
+              )}
+              <TextButton
+                text={accounts.length === 0 ? '계좌 연결하기' : '참여 완료하기'}
+                onClickHandler={handleSelectAccntDone}
+                isDisabled={accounts.length === 0 ? false : !selectedAccntId}
+              />
+            </>
           ) : (
-            <AccountSelect accounts={accounts} accountSelectHandler={handleSelectAccnt} />
+            <>
+              <Info>
+                등록한 계좌를 이미 사용 중입니다.
+                <br />
+                계좌는 최대 1개까지 등록할 수 있습니다.
+              </Info>
+            </>
           )}
-          <TextButton
-            text={accounts.length === 0 ? '계좌 연결하기' : '참여 완료하기'}
-            onClickHandler={handleSelectAccntDone}
-            isDisabled={accounts.length === 0 ? false : !selectedAccntId}
-          />
         </Content>
       </ModalBox>
     </>
