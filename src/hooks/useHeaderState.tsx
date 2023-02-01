2aa8a0670581e2f697a0ab7d7c94d1fe83373808
ptg0811 <ptg0811@gmail.com>
2023-02-01 22:15:12 +0900

    FEAT: add setting icon button in header #157

--- src/hooks/useHeaderState.tsx
+++ src/hooks/useHeaderState.tsx
@@ -43,11 +43,19 @@ const useHeaderState = ({ pathname }: { pathname: string }) => {
     setShowPrevBtn(false);
   };
 
+  const { id } = useRecoilValue(userId);
+
+  const [showSettingsBtn, setShowSettingsBtn] = useState<boolean>(false);
+  const handleSettingsClick = () => {
+    navigate(`/users/settings/${id}`);
+  };
+
   const showPrevOnly = () => {
     setShowPrevBtn(true);
     setShowSearchBar(false);
     setShowSearchBtn(false);
     setShowChatBtn(false);
+    setShowSettingsBtn(false);
   };
 
   const showChatOnly = () => {
@@ -55,23 +63,31 @@ const useHeaderState = ({ pathname }: { pathname: string }) => {
     setShowSearchBar(false);
     setShowSearchBtn(false);
     setShowChatBtn(true);
+    setShowSettingsBtn(false);
   };
 
-  const { id } = useRecoilValue(userId);
+  const showSettingsOnly = () => {
+    setShowPrevBtn(false);
+    setShowSearchBar(false);
+    setShowSearchBtn(false);
+    setShowChatBtn(false);
+    setShowSettingsBtn(true);
+  };
+
   useEffect(() => {
     if (pathname === '/home') {
       showChatOnly();
       return;
     }
-    if (pathname === `/users/${id}`) {
-      showChatOnly();
-      return;
-    }
-    if (pathname.includes('/users/')) {
-      showPrevOnly();
+    if (pathname.includes('/users') && !pathname.includes('/edit') && !pathname.includes('/settings')) {
+      showSettingsOnly();
       return;
     }
     if (pathname.includes('/edit')) {
+      showPrevOnly();
+      return;
+    }
+    if (pathname.includes('/settings')) {
       showPrevOnly();
       return;
     }
@@ -111,11 +127,13 @@ const useHeaderState = ({ pathname }: { pathname: string }) => {
     showPrevBtn,
     showSearchBar,
     keyword,
+    showSettingsBtn,
     handleSearchClick,
     handleChatClick,
     handlePrevClick,
     handleKeywordChange,
     handleKeypress,
+    handleSettingsClick,
   };
 };
 
